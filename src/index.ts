77964a003a997414df6c27c4cc68ff2edba9e08f
Anna Kryva <anna120100@gmail.com>
2021-10-17 23:18:08 +0300

    fix: remove extra slash

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export {default as usePushNotifications} from './hooks//usePushNotifications';
+export {default as usePushNotifications} from './hooks/usePushNotifications';
 export {default as useInitNotifications} from './hooks/useInitNotifications';
 
 export {configurePushNotifications} from './utils/pushNotifications/index';
